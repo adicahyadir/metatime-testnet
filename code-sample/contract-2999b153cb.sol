715abe0aaef551e5f394e5221062b79cbf642439
adicahyadir <adicr1999@gmail.com>
2023-06-26 12:51:03 +0700

    Token Test Metachain

---
+++ code-sample/contract-2999b153cb.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts@4.9.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.2/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.2/access/Ownable.sol";
+
+contract AsmoToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("AsmoToken", "ATK") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
